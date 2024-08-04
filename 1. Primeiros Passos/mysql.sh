f030d17cbcc54b8b050f06712297aeb622e4dc61
viniventura <vini.ventura.ti@hotmail.com>
2024-08-04 17:00:52 -0300

    Comando pra levantar container MySQL para desenvolvimento

---
+++ 1. Primeiros Passos/mysql.sh
@@ -0,0 +1,2 @@
+# MySQL container
+docker run --name mysql-dev -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root1234 -e MYSQL_DATABASE=docker_db -e MYSQL_USER=docker_usr -e MYSQL_PASSWORD=docker_pwd  mysql
